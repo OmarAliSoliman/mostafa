8fc968a4e3bd2f1a3537d8045a72f1b115d22459
OmarAliSoliman <49602133+OmarAliSoliman@users.noreply.github.com>
2020-06-10 10:30:48 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-    scrollHorizontally: true,
-    navigation: true,
-    navigationTooltips: ['Home', 'About', 'Project', 'Contact'],
-    css3: true,
-    scrollingSpeed: 1500,
-    animateAnchor: true,
-	});
+$(document).ready(function () {
+  $(".nav-list li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+
+  $(window).scroll(()=>{
+    if($(this).scrollTop() > 20){
+      $('.nav').addClass('fixed-navbar');
+      $('#floating').css('opacity', '0.8');
+    }else{
+      $('.nav').removeClass('fixed-navbar');
+      $('#floating').css('opacity', '0');
+    }
+  })
+
+
+  $('#floating').click(()=>{
+    $('html, body').animate({scrollTop: 0}, 50);
+  })
+
+
 });
