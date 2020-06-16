4b576280d5ef94fd993f91a09c309160f998e723
OmarAliSoliman <omar.ali.alasmar@gmail.com>
2020-06-16 21:14:37 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-  $(".nav-list li").click(function () {
+  $(".navbar li").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
   });
 
   $(window).scroll(()=>{
     if($(this).scrollTop() > 20){
-      $('.nav').addClass('fixed-navbar');
+      $('.navbar').addClass('fixed-navbar');
       $('#floating').css('opacity', '0.8');
     }else{
-      $('.nav').removeClass('fixed-navbar');
+      $('.navbar').removeClass('fixed-navbar');
       $('#floating').css('opacity', '0');
     }
   })
@@ -18,5 +18,57 @@ $(document).ready(function () {
     $('html, body').animate({scrollTop: 0}, 50);
   })
 
+
+  
+  var typed = new Typed('.type', {
+    strings: [ 
+      " Mostafa Mahmoud",
+      " an Engineer",
+      " Motion Graphic",
+    ],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true
+  });
+
+
+  $('.project-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    loop: true,
+    arrows: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        }
+      }
+    ]
+  });
 
 });
